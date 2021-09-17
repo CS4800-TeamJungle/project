aed35137e25198bc3dc6a7582fd81a9dfe13e97c
Andy Peng <48707379+sjpmpzx@users.noreply.github.com>
2021-09-16 17:28:51 -0700

    Add another hello world api

--- backend/backend.py
+++ backend/backend.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,5 +7,9 @@ CORS(app)
 @app.route('/helloworld', methods=['GET'])
 def test():
     return "Hello World"
+
+@app.route('/hello', methods=['GET'])
+def test_2():
+    return f"Hello, {request.args.get('name', default='anonymous', type=str)}!"
 
 app.run()
