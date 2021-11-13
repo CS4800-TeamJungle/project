56a824033ac3895372097052672060091a27559d
kkwik <kevinkwik6@gmail.com>
2021-11-13 13:14:02 -0800

    Added bootstrap and styled page header

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,15 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div class="header-container container-fluid">
+      <header class="d-flex justify-content-center py-2">
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/inventory"
+              >Inventory</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/search"
+              >Search Recipes</router-link
+            >
+          </li>
+        </ul>
+      </header>
+    </div>
+
+    <!--div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/inventory">Inventory</router-link> |
       <router-link to="/search">Search Recipes</router-link>
-    </div>
+    </div -->
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+body {
+  background-color: #e3e3e3;
+}
+
+div.container-fluid {
+  background-color: white;
+  box-shadow: 0px 5px 5px;
+}
+
+div.header-container {
+  margin-bottom: 10px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,15 +51,11 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
+.nav-pills {
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
     &.router-link-exact-active {
-      color: #42b983;
+      color: #fff;
+      background-color: #0d6efd;
     }
   }
 }
